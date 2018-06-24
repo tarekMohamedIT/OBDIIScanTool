c3b695dcf00a6ba8e708e1dac859cb7e7d1d259b
r3tr0 <tarekmohamedit@gmail.com>
2018-06-24 17:06:20 +0200

    Updated the service completely.

--- app/src/main/java/Commands/FuelLevel.java
+++ app/src/main/java/Commands/FuelLevel.java
@@ -1,13 +1,23 @@
 package Commands;
 
-import communications.ObdService;
+import android.content.Context;
 
-public class FuelLevel extends ObdService {
+import helpers.BaseObdCommand;
 
-
-
-
+public class FuelLevel extends BaseObdCommand<Integer> {
 
 
+    public FuelLevel(Context context, String commandString) {
+        super(context, commandString);
+    }
 
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public Integer performCalculations(byte[] bytes) {
+        return null;
+    }
 }
